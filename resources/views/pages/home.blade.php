7f03d6ba59b88b8548309d920282da37f67a08a7
Anthony Najjar <anthonynajjars@gmail.com>
2016-03-17 19:17:29 +0100

    Reset repo

--- resources/views/pages/home.blade.php
+++
@@ -1,4 +0,0 @@
-@extends('layouts.default')
-@section('content')
-    Wlp Build Web Home
-@stop
--- resources/views/project/form.blade.php
+++
@@ -1,7 +0,0 @@
-<div calss="form-group">
-	{!! Form::label('name', 'Name:') !!}
-	{!! Form::text('name', NULL, ['class' => 'form-control']) !!}
-</div>	
-<div calss="form-group">
-	{!! Form::submit($submitText, ['class' => 'btn btn-primary form-controll']) !!}
-</div>
