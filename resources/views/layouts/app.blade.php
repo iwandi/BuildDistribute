3a0b1d407369a32d1f1c9a38df660cdfd4c9aa40
Anthony Najjar <anthonynsimon@gmail.com>
2016-03-21 14:37:17 +0100

    Cleaning up unsused dependencies

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -90,8 +90,6 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js"></script>
 	<script src="{{url('js/pushy.min.js')}}"></script>
-	<script src="{{url('js/tableSorter.js')}}"></script>
-
 </body>
 
 </html>
